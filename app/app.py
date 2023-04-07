528b29dc0d2e154b6972ce0999a023aa4c04b2c1
KD131 <71063280+KD131@users.noreply.github.com>
2023-04-08 01:50:28 +0200

    game recommendation query

--- app/app.py
+++ app/app.py
@@ -1,10 +1,36 @@
+import pandas as pd
 from graphdatascience import GraphDataScience
 from modules.secrets import NEO4J_AUTH, NEO4J_URI
 
 
+def game_recommendations(gds: GraphDataScience, game_title: str, limit: int=10) -> pd.DataFrame:
+    """Recommend games similar to the given game title."""
+    return gds.run_cypher(
+        """
+        match (s:Game {title: $title})<-[:PLAY]-(u:User)-[:PLAY]->(t:Game)
+        where (s)-->(:Genre)<--(t)
+        return t.title as title, count(*) as cnt
+        order by cnt desc
+        limit $limit
+        """,
+        params={
+            "title": game_title,
+            "limit": limit
+        },
+        database='neo4j'
+    )
+
 def main():
-    gds = GraphDataScience(NEO4J_URI, NEO4J_AUTH)
-    print(gds.version())
+    gds = GraphDataScience(NEO4J_URI, NEO4J_AUTH, database='neo4j')
+    print(f"Connected to Neo4j database version {gds.version()}")
+    while True:
+        game_title = input("Enter a game title (or 'quit' to exit): ")
+        if game_title == "quit":
+            break
+        else:
+            games = game_recommendations(gds, game_title)
+            print(games)
+    gds.close()
 
 if __name__ == "__main__":
     main()
