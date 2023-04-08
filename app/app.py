507a6607b84076b5782a02c7c2504606865f40e2
KD131 <71063280+KD131@users.noreply.github.com>
2023-04-08 20:44:40 +0200

    adds eigenvector graph algorithm

--- app/app.py
+++ app/app.py
@@ -23,12 +23,31 @@ def game_recommendations(gds: GraphDataScience, game_title: str, limit: int=10) 
     games.index += 1
     return games
 
-def main():
-    gds = GraphDataScience(NEO4J_URI, NEO4J_AUTH, database='neo4j')
-    print(f"Connected to Neo4j database version {gds.version()}")
+def project_graph(gds: GraphDataScience):
+    return gds.graph.project(
+        'games',
+        ['Game', 'User'],
+        ['PLAY', 'PURCHASE'],
+        relationshipProperties={
+            'hours': { 'defaultValue': 1.0 }
+        }
+    )
+
+def most_important_games(gds: GraphDataScience, G, limit: int=10):
+    df = gds.eigenvector.stream(G, relationshipWeightProperty='hours')
+    df.sort_values(by='score', ascending=False, inplace=True)
+    df = pd.DataFrame(df.head(limit))
+    df.index = pd.RangeIndex(start=1, stop=len(df)+1)   # this is also limit + 1
+    ids = df['nodeId'].to_list()
+    nodes = gds.util.asNodes(ids)
+    titles = [node['title'] for node in nodes]
+    df.insert(1, 'Title', titles)
+    return df
+
+def game_recommendation_menu(gds: GraphDataScience):
     while True:
-        game_title = input("Enter a game title (or 'quit' to exit): ")
-        if game_title == "quit":
+        game_title = input("Enter a game title (or '0' to exit): ")
+        if game_title == '0':
             break
         else:
             games = game_recommendations(gds, game_title)
@@ -36,6 +55,31 @@ def main():
                 print(f"No recommendations for {game_title}")
             else:
                 print(games)
+
+def main():
+    gds = GraphDataScience(NEO4J_URI, NEO4J_AUTH, database='neo4j')
+    G = None
+
+    print(f"Connected to Neo4j database version {gds.version()}")
+    while True:
+        print("1. Game recommendations")
+        print("2. Most 'important' games")
+        print("0. Exit (or 'quit', '\q')")
+        command = input("Please select an option: ")
+        
+        if command in ['0', 'quit', '\q']:
+            break
+        elif command == '1':
+            game_recommendation_menu(gds)
+        elif command == '2':
+            if G is None:
+                try:
+                    G = gds.graph.get('games')
+                except:
+                    G, _ = project_graph(gds)
+            games = most_important_games(gds, G)
+            print(games)
+        
     gds.close()
 
 if __name__ == "__main__":
